d2e189abed74526312e80335d04ceedf3e4063d9
litalkaufman <litalkaufman@gmail.com>
2015-09-07 22:33:45 +0300

    tags to my page

--- includes/mypage.js
+++ includes/mypage.js
@@ -1,4 +1,5 @@
 var user;
+var allTagsDict = {};
 $(document).on(
 	'parseload',  //  <---- HERE'S OUR CUSTOM EVENT BEING LISTENED FOR
 	function(res){
@@ -18,6 +19,8 @@ $(document).on(
 			
 });
 
+
+
 function getCheckins(){
 	var CheckIn = Parse.Object.extend('CheckIn');
 	var query = new Parse.Query(CheckIn);
@@ -28,10 +31,25 @@ function getCheckins(){
 	query.limit(5);
 	query.find({
 	  success: function(results) {
-	  	fillMyPageContent(results);
+	  	getTags(results);
 	  },
 	  error: function(error) {
 	    // alert("Error: " + error.code + " " + error.message);
+	  }
+	});
+}
+
+function getTags(results){
+	var Tag = Parse.Object.extend("Tag");
+	var query = new Parse.Query(Tag);
+	query.find({
+	  success: function(allTags) {
+	  	var addRows    = {};
+		 	  	 
+	  	for (var i=0; i < allTags.length ; i++) {
+	  		allTagsDict[allTags[i].id] = allTags[i];
+ 		}
+ 		fillMyPageContent(results);
 	  }
 	});
 }
@@ -41,6 +59,8 @@ function fillMyPageContent(res){
 	  var checkIn = res[i];
 	  var when = checkIn.get("when").format("dd/m/yy");
 	  var place = checkIn.get("place");
+	  
+	  
 	  var dis = calcDistance(place.get("address_geo").latitude, place.get("address_geo").longitude);
 	  if (dis>=1){
 		  dis = dis.toFixed(2) + " km";
@@ -48,7 +68,24 @@ function fillMyPageContent(res){
 	  	  dis = dis.toFixed(3) * 1000 + " m";
 	  }
 	  
-	  $("#checkins").append("<div>" + checkIn.get("place").get("name") + "</div>" + "<div>"+ when+ "</div>"+"<div>distance:"+ dis+ "</div>");
+	  var tags = place.get("tags");
+  	  var tagsDiv = "";
+	  if (tags){
+	  	tagsDiv = $("<div><div>");
+	  	
+	  	for (var i=0; i < tags.length; i++) {
+			var tag = tags[i];
+			var name = allTagsDict[tag].get("name");
+			var span    = $('<span></span>');
+			span   .html(name);
+			tagsDiv.append(span);
+		  };
+	  }
+	  $("#checkins").append("<div>" + checkIn.get("place").get("name") + "</div>");
+	  $("#checkins").append("<div>"+ when+ "</div>");
+  	  $("#checkins").append(tagsDiv);
+  	  $("#checkins").append("<div>distance:"+ dis+ "</div>");
+
 	  
 	};
 }
