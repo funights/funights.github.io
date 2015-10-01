9f4546dc75da286b305797fd4e168daa06ac4287
michal1elbaz <michal1elbaz@gmail.com>
2015-10-01 21:57:05 +0300

    z
    z

--- includes/top.js
+++ includes/top.js
@@ -1,23 +1,8 @@
 savedPosition = false;
 
 function showError(error) {
-    switch(error.code) {
-        case error.PERMISSION_DENIED:
-        	if( !getCookie( 'approvedLocation' )) {
-            	alert("בבקשה אפשר גישה לשירותי מיקום");
-            	setCookie( 'approvedLocation', 'true' );
-           }
-            break;
-        case error.POSITION_UNAVAILABLE:
-          alert("Location information is unavailable.");
-            break;
-        case error.TIMEOUT:
-           alert("The request to get user location timed out.");
-            break;
-        case error.UNKNOWN_ERROR:
-          alert("An unknown error occurred.");
-            break;
-    }
+	alert( error.code );
+
 }
 
 function savePosition(position){
@@ -39,6 +24,7 @@ function getLocation(){
         navigator.geolocation.getCurrentPosition(savePosition, showError);
     } else {
       //  setTimeout( "getLocation()", 0 );
+      alert( "no navigator" );
     }
 }
 
