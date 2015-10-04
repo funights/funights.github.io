5f9909b82635173fb41a619eb1b4993d6a7ac0c5
michal1elbaz <michal1elbaz@gmail.com>
2015-10-04 18:41:06 +0300

    1

--- includes/parse_main.js
+++ includes/parse_main.js
@@ -23,7 +23,7 @@ function getPlacesSuccess(res){
 	var compiled = _.template(template);
 	
 	$("#target").html(compiled({items:closePlaces}) );
-	 alert("Hello"); }, 3000);
+	 alert("Hello"); }, 5000);
 }
 
 $(document).on(
