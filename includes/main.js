500e2cf66b3b8a76c5a2828de998458e39570a75
michal1elbaz <michal1elbaz@gmail.com>
2015-10-04 17:41:28 +0300

    2

--- includes/main.js
+++ includes/main.js
@@ -132,11 +132,12 @@ function savePosition(position){
 Number.prototype.toRad = function() { return this * (Math.PI / 180); };
 function calcDistance(lat2, lon2){
    	
-    while (!savedPosition){
+    if (!savedPosition){
     	getLocation();
+        return 0;
     }
     
-
+    else{
 	    var lat1 = savedPosition.coords.latitude;
 	    var lon1 = savedPosition.coords.longitude;
 	    var R = 6371; // km
@@ -150,7 +151,7 @@ function calcDistance(lat2, lon2){
 	    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
 	    var d = R * c;
 	    return d;
-
+	   }
 }
 
 function showError(error) {
